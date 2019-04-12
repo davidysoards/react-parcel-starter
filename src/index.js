42e6f9a93962245a74d50584e1ed9ed774c7789f
David Soards <davids@gocodigo.com>
2019-04-12 16:52:00 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'normalize.css';
+import './index.scss';
+import App from './js/App';
+
+render(<App />, document.getElementById('root'));
